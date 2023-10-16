10ebb90dc615c4e1a8e94057f8d16c9fc2a05a2b
shahad <shahad@42.solutions>
2023-10-16 11:03:59 +0300

    Counter App using redux

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { decrement, increment, reset} from './counterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+
+const Counter = () =>{
+    const count = useSelector((state: any) => state.counterR.count);
+    
+    const dispatch = useDispatch();
+
+    
+    const handleIncrement = () => {
+        dispatch(increment());
+    }
+    const handleDecrement = () => {
+        dispatch(decrement());
+    }
+    const handleReset = () => {
+        dispatch(reset());
+    }
+
+
+    return (
+        <div>
+           <h1>Count: {count}</h1>
+           <button onClick={handleIncrement}>Increment</button> 
+           <button onClick={handleReset}>Reset</button> 
+           <button onClick={handleDecrement}>Decrement</button> 
+        </div>
+    )
+}
+
+export default Counter;
---
+++ src/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./components/counter/counterSlice";
+
+export const store = configureStore({
+    reducer: {
+        counterR: counterReducer,
+    }
+})
+
